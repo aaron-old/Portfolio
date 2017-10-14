5a0fbe395a8b92d665210c9ea7158e88fbdad404
Aaron <mustarddevelopment@gmail.com>
2017-10-14 16:12:33 -0700

    Removing unused styling from router.

--- client/src/components/Router/PageRouter.js
+++ client/src/components/Router/PageRouter.js
@@ -2,11 +2,6 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import routes from './Routes';
 
-const styles = {
-    display: {
-        display: "inline-block"
-    }
-};
 
 const PageRouter = () => {
 
